0db0133e2e428066b37bdd19215aa94094198110
“DiwanShubham” <shubhamsdiwan1993@gmail.com>
2018-06-26 11:23:03 +0530

    Factorial program without scanf function

---
+++ factorial.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(int argc, char* argv[])
+{
+	int i;
+        unsigned long long fact=1;
+	int d=1;
+	
+
+	if (argc < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+
+    else
+    {
+        for(i=1; i<=argc; ++i)
+        {
+            fact *= i;              
+        }
+        printf("Factorial of %d = %llu", argc, fact);
+    }
+
+	printf("\n%d\n",argc);
+}
+
+
+
+
